dfc1bdac10a53bd165cdf663383194fbcffbb668
Angel <153700634+xHianx@users.noreply.github.com>
2024-11-13 10:10:35 -0500

    fix some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Grid from '@mui/material/Grid2'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Grid container spacing={5}>
 
