bcfc70b7a5a871f75836a2575263291061d3e6b0
Angel <153700634+xHianx@users.noreply.github.com>
2024-11-25 10:06:17 -0500

    hola

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
 import './App.css'
 import Grid from '@mui/material/Grid2'
+import IndicatorWeather from './Components/IndicatorWeather'
+import TableWeather from './Components/TableWeather';
+import ControlWeather from './Components/ControlWeather';
+import LineChartWeather from './Components/LineChartWeather';
 
 function App() {
   return (
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-      <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+      </Grid>
+
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+      </Grid>
+
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+      </Grid>
+
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+      </Grid>
 
       {/* Tabla */}
-      <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
+      <Grid size={{ xs: 12, xl: 8 }}>
+        {/* Grid Anidado */}
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, xl: 3 }}>
+            <ControlWeather />
+          </Grid>
+
+          <Grid size={{ xs: 12, xl: 9 }}>
+            <TableWeather />
+          </Grid>
+
+        </Grid>
+      </Grid>
 
       {/* Gráfico */}
-      <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
-
-    </Grid>
+      <Grid size={{ xs: 12, xl: 4 }}>
+        <LineChartWeather />
+      </Grid>
+      
+    </Grid >
   )
 }
 
